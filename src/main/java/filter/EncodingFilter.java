1a8e19772b02d4e56633b347782d7668ba4fe0d1
22222jh <937254522@qq.com>
2019-02-19 21:56:24 +0800

    垃圾模板ver2.0

--- src/main/java/filter/EncodingFilter.java
+++ src/main/java/filter/EncodingFilter.java
@@ -1,7 +1,5 @@
 package filter;
 
-import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
-
 import javax.servlet.*;
 import java.io.IOException;
 
@@ -9,11 +7,18 @@ import java.io.IOException;
  * 编码格式拦截器
  * @author 22222jh
  * */
-public class EncodingFilter extends UrlRewriteFilter {
+public class EncodingFilter implements Filter {
 
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException  {
         req.setCharacterEncoding("UTF-8");
-        super.doFilter(req,resp,chain);
+        chain.doFilter(req,resp);
+    }
+
+    public void destroy() {
+
     }
 }
